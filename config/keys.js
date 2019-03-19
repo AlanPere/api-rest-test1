73be383cc16f95ad7453dbcfcd4ec38e288bc3da
Alan Joaquín Peré <apere@huenei.com>
2019-03-19 10:36:39 -0300

    primer subida

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector'
+// };
+
+module.exports = {
+  mongoURI: 'mongodb://user123:user123@ds221405.mlab.com:21405/pruebamern',
+  secretOrKey: 'secret'
+};
+
+
